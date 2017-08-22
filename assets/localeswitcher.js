9332732abcaee81f6d5f66b2d4f270d9ed79e7f8
Richard Verbruggen <richard@sturb.nl>
2017-08-22 19:55:01 +0200

    Update 1.0.7

--- assets/localeswitcher.js
+++ assets/localeswitcher.js
@@ -1,14 +1,15 @@
 var select = document.getElementById('vannut_localeswitcher');
 
-select.addEventListener('change', function (e){
-    e.preventDefault();
-    var gekozen = select.options[select.selectedIndex];
-    var locale = gekozen.value;
+if (select) {
+    select.addEventListener('change', function (e){
+        e.preventDefault();
+        var gekozen = select.options[select.selectedIndex];
+        var locale = gekozen.value;
 
-    var uri = window.btoa(gekozen.dataset.url);
+        var uri = window.btoa(gekozen.dataset.url);
 
-    console.log(uri);
-    window.location.href = '/switch_locale/'+locale+'/'+uri;
+        window.location.href = '/switch_locale/'+locale+'/'+uri;
 
 
-}, true)
+    }, true)
+}
